aba2b786cce0107cc4de5ffa894ad710e71cf2cd
Kinsley <kajivakinsley@gmail.com>
2026-02-05 17:18:40 +0200

    add files

--- init_deps.sh
+++ init_deps.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 set -e
 
-# Clone dependencies
+# Clone dependencies (overwrite if exists)
+for dir in ogg opus-1.6.1 libopusenc opusfile opus-tools; do
+    if [ -d "$dir" ]; then
+        echo "Removing existing directory: $dir"
+        rm -rf "$dir"
+    fi
+done
+
 git clone --depth 1 https://github.com/xiph/ogg.git ogg
 git clone --depth 1 https://github.com/xiph/opus.git opus-1.6.1
 git clone --depth 1 https://github.com/xiph/libopusenc.git libopusenc
